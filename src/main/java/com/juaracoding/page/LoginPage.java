8b7f89c5d09e4f72167e28da1960e11e73770fca
cfra321 <kukuhwicaksono105xb@gmail.com>
2023-03-02 18:24:12 +0700

    fist commit

---
+++ src/main/java/com/juaracoding/page/LoginPage.java
@@ -0,0 +1,43 @@
+package com.juaracoding.page;
+
+import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    private WebDriver driver;
+    public LoginPage() {
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "(//h2[normalize-space()='Login'])[1]")
+    WebElement loginTitle;
+    @FindBy(id = "username")
+    WebElement username;
+    @FindBy(id = "password")
+    WebElement password;
+    @FindBy(id = "rememberme")
+    WebElement btnrememberme;
+    @FindBy(xpath = "(//button[normalize-space()='Log in'])[1]")
+    WebElement  btnLogin;
+    @FindBy(xpath = "//div[@id='primary']//li[1]")
+    WebElement txtInvalidCredentials;
+
+    public String getLoginTitle(){
+        return loginTitle.getText();
+    }
+
+    public String getTxtInvalidCredentials(){
+        return txtInvalidCredentials.getText();
+    }
+
+    public void login(String username, String password){
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+        btnrememberme.click();
+        btnLogin.click();
+    }
+}
---
+++ src/test/java/com/juaracoding/stepdefinitions/TestLogin.java
@@ -0,0 +1,51 @@
+package com.juaracoding.stepdefinitions;
+
+import com.juaracoding.drivers.DriverSingleton;
+import com.juaracoding.page.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestLogin {
+
+    public static WebDriver driver;
+    public LoginPage loginPage;
+
+    @BeforeClass
+    public void setUp(){
+        DriverSingleton.getInstance("chrome");
+        driver = DriverSingleton.getDriver();
+        loginPage = new LoginPage();
+    }
+
+    @Test(priority = 1)
+    public void testurl(){
+        driver.get("https://shop.demoqa.com/my-account/");
+
+        //step verify
+        Assert.assertEquals(loginPage.getLoginTitle(),"LOGIN");
+    }
+
+    @Test(priority = 2)
+    public void testInvalidLogin() {
+        delay(1);
+        loginPage.login("kukuh", "invalid");
+        Assert.assertEquals(loginPage.getTxtInvalidCredentials(), "ERROR: The username or password you entered is incorrect. Lost your password?");
+    }
+    @AfterClass
+    public void quitBrowser(){
+        delay(2);
+        DriverSingleton.closeObjectInstance();
+    }
+
+    static void delay(long detik){
+        try {
+            Thread.sleep(detik*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
